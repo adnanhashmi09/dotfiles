8e1d1156eea7cafdafa58ef97441f7c91cb717f2
Adnan Hashmi <hashmi.adnan791@gmail.com>
2024-05-26 13:07:46 +0530

    config files for vim and tmux
    Signed-off-by: Adnan Hashmi <hashmi.adnan791@gmail.com>

---
+++ .config/lvim/config.lua
@@ -0,0 +1,504 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+--[[
+lvim is the global options object
+
+Linters should be
+filled in as strings with either
+a global executable or a path to
+an executable
+]]
+-- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
+-- general
+lvim.log.level = "warn"
+lvim.format_on_save.enabled = true
+lvim.colorscheme = "lunar"
+lvim.transparent_window = true
+vim.diagnostic.config({ virtual_text = false })
+
+-- to disable icons and use a minimalist setup, uncomment the following
+-- lvim.use_icons = false
+
+-- keymappings [view all the defaults by pressing <leader>Lk]
+lvim.leader = "space"
+-- add your own keymapping
+lvim.number = false
+
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode["<S-l>"] = ":bnext<CR>"
+lvim.keys.normal_mode["<S-h>"] = ":bprevious<CR>"
+lvim.keys.normal_mode["<leader>m"] = ":NvimTreeFocus<CR>"
+lvim.keys.insert_mode["jk"] = "<ESC>"
+lvim.keys.normal_mode["<leader>tl"] = ":tabn<CR>"
+lvim.keys.normal_mode["<leader>tp"] = ":tabN<CR>"
+
+lvim.keys.normal_mode["<c-h>"] = "<cmd><C-U>TmuxNavigateLeft<cr>"
+lvim.keys.normal_mode["<c-l>"] = "<cmd><C-U>TmuxNavigateRight<cr>"
+lvim.keys.normal_mode["<c-j>"] = "<cmd><C-U>TmuxNavigateDown<cr>"
+lvim.keys.normal_mode["<c-k>"] = "<cmd><C-U>TmuxNavigateUp<cr>"
+
+-- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+-- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+-- unmap a default keymapping
+-- vim.keymap.del("n", "<C-Up>")
+-- override a default keymapping
+-- lvim.keys.normal_mode["<C-q>"] = ":q<cr>" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
+
+-- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
+-- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
+-- local _, actions = pcall(require, "telescope.actions")
+-- lvim.builtin.telescope.defaults.mappings = {
+--   -- for input mode
+--   i = {
+--     ["<C-j>"] = actions.move_selection_next,
+--     ["<C-k>"] = actions.move_selection_previous,
+--     ["<C-n>"] = actions.cycle_history_next,
+--     ["<C-p>"] = actions.cycle_history_prev,
+--   },
+--   -- for normal mode
+--   n = {
+--     ["<C-j>"] = actions.move_selection_next,
+--     ["<C-k>"] = actions.move_selection_previous,
+--   },
+-- }
+--
+--
+-- Base vim settings
+
+local o = vim.o
+
+o.relativenumber = true
+o.guicursor = ""
+
+o.incsearch = true
+o.hlsearch = false
+
+o.showmode = true
+
+
+-- Change theme settings
+-- lvim.builtin.theme.options.dim_inactive = true
+-- lvim.builtin.theme.options.style = "storm"
+
+-- Use which-key to add extra bindings with the leader-key prefix
+-- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+-- lvim.builtin.which_key.mappings["t"] = {
+--   name = "+Trouble",
+--   r = { "<cmd>Trouble lsp_references<cr>", "References" },
+--   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+--   d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+--   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+--   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+--   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
+-- }
+
+-- TODO: User Config for predefined plugins
+-- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+lvim.builtin.terminal.open_mapping = [[\\]]
+lvim.builtin.terminal.direction = "horizontal"
+lvim.builtin.terminal.shade_terminals = true
+lvim.builtin.terminal.shading_factor = 2
+lvim.builtin.terminal.size = 25
+
+vim.keymap.set('t', "jk", "<C-\\><C-n>")
+-- vim.keymap.set('n', "<C-\\>", ":ToggleTerm 20 direction=float<CR>")
+-- vim.keymap.set('t', "<C-\\>", "<C-\\><C-n>:ToggleTerm 20 direction=float<CR>")
+-- vim.keymap.set('i', "<C-\\>", "<C-\\><C-n>:ToggleTerm 20 direction=float<CR>")
+-- vim.keymap.set('n', [[\\]], ":ToggleTerm direction=horizontal<CR>")
+-- vim.keymap.set('t', [[\h]], ":ToggleTerm direction=horizontal<CR>")
+-- vim.keymap.set('i', [[\\]], ":ToggleTerm direction=horizontal<CR>")
+
+lvim.builtin.which_key.mappings["r"] = {
+  name = "Run",
+  s = {
+    '<cmd>autocmd bufwritepost [^_]*.sass,[^_]*.scss  silent exec "!sass %:p %:r.css"<CR>',
+    "Auto Compile Sass",
+  },
+  r = { "<cmd>RunCode<CR>", "Run Code" },
+  f = { "<cmd>RunFile<CR>", "Run File" },
+  p = { "<cmd>RunProject<CR>", "Run Project" },
+  -- g = { "<cmd>ToggleTerm size=70 direction=vertical<cr>gradle run<cr>", "Run Gradle" },
+  -- m = {
+  --   "<cmd>ToggleTerm size=70 direction=vertical<cr>mvn exec:java -Dexec.mainClass=com.pojokcode.App<cr>",
+  --   "Run MVN",
+  -- },
+}
+
+-- lvim.builtin.alpha.dashboard.section.footer.val = footer()
+-- if you don't want all the parsers change this to a table of the ones you want
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "c",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "tsx",
+  "css",
+  "rust",
+  "java",
+  "yaml",
+  "go",
+  "gomod",
+}
+
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enable = true
+
+-- generic LSP settings
+
+-- -- make sure server will always be installed even if the server is in skipped_servers list
+lvim.lsp.installer.setup.ensure_installed = {
+  "lua_ls",
+  "jsonls",
+  "tsserver",
+  "clangd",
+  "pyright"
+}
+-- -- change UI setting of `LspInstallInfo`
+-- -- see <https://github.com/williamboman/nvim-lsp-installer#default-configuration>
+-- lvim.lsp.installer.setup.ui.check_outdated_servers_on_open = false
+-- lvim.lsp.installer.setup.ui.border = "rounded"
+-- lvim.lsp.installer.setup.ui.keymaps = {
+--     toggle_server_expand = "o",
+--     uninstall_server = "d",
+-- }
+
+-- ---@usage disable automatic installation of servers
+-- lvim.lsp.installer.setup.automatic_installation = false
+
+-- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
+-- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
+-- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
+-- local opts = {} -- check the lspconfig documentation for a list of all possible options
+-- require("lvim.lsp.manager").setup("pyright", opts)
+
+-- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
+-- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
+-- lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
+--   return server ~= "emmet_ls"
+-- end, lvim.lsp.automatic_configuration.skipped_servers)
+
+-- -- you can set a custom on_attach function that will be used for all the language servers
+-- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
+-- lvim.lsp.on_attach_callback = function(client, bufnr)
+--   local function buf_set_option(...)
+--     vim.api.nvim_buf_set_option(bufnr, ...)
+--   end
+--   --Enable completion triggered by <c-x><c-o>
+--   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+-- end
+
+-- -- set a formatter, this will override the language server formatting capabilities (if it exists)
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  {
+    command = "rustfmt",
+    extra_args = { "--edition", "2021" },
+  },
+  { command = "black",       filetypes = { "python" } },
+  { command = "goimports",   filetypes = { "go" } },
+  { command = "gofumpt",     filetypes = { "go" } },
+  { command = "clang_format" },
+  { command = "prettier", },
+}
+-- local formatters = require "lvim.lsp.null-ls.formatters"
+-- formatters.setup {
+--   { command = "black", filetypes = { "python" } },
+--   { command = "isort", filetypes = { "python" } },
+--   {
+--     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+--     command = "prettier",
+--     ---@usage arguments to pass to the formatter
+--     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+--     extra_args = { "--print-with", "100" },
+--     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+--     filetypes = { "typescript", "typescriptreact" },
+--   },
+-- }
+
+-- -- set additional linters
+-- local linters = require "lvim.lsp.null-ls.linters"
+-- linters.setup {
+-- { command = "flake8", filetypes = { "python" } },
+-- {
+--   -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+--   command = "shellcheck",
+--   ---@usage arguments to pass to the formatter
+--   -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+--   extra_args = { "--severity", "warning" },
+-- },
+-- {
+--   command = "codespell",
+--   ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+--   filetypes = { "javascript", "python" },
+-- },
+-- }
+
+-- Additional Plugins
+-- python environment switcher
+
+lvim.plugins = {
+  "AckslD/swenv.nvim",
+  "stevearc/dressing.nvim",
+  "olexsmir/gopher.nvim",
+  "leoluz/nvim-dap-go",
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+  },
+  {
+    "CRAG666/code_runner.nvim",
+    dependencies = "nvim-lua/plenary.nvim",
+    config = function()
+      require('code_runner').setup({
+        mode = "toggleterm",
+        term = {
+          size = 25
+        },
+        filetype = {
+          python = "python3 -u",
+          typescript = "node run",
+          rust = {
+            "cd $dir &&",
+            "rustc $fileName &&",
+            "$dir/$fileNameWithoutExt"
+          },
+        },
+      })
+    end,
+  },
+  {
+    "amitds1997/remote-nvim.nvim",
+    version = "*",                     -- Pin to GitHub releases
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- For standard functions
+      "MunifTanjim/nui.nvim",          -- To build the plugin UI
+      "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+    },
+    config = true,
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  }
+}
+
+require('swenv').setup({
+  post_set_venv = function()
+    vim.cmd("LspRestart")
+  end,
+})
+
+lvim.builtin.which_key.mappings["C"] = {
+  name = "Python",
+  c = { "<cmd>lua require('swenv.api').pick_venv()<cr>", "Choose Env" },
+}
+
+-- Autocommands (https://neovim.io/doc/user/autocmd.html)
+-- vim.api.nvim_create_autocmd("BufEnter", {
+--   pattern = { "*.json", "*.jsonc" },
+--   -- enable wrap mode for json files only
+--   command = "setlocal wrap",
+-- })
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "zsh",
+--   callback = function()
+--     -- let treesitter use bash highlight for zsh files as well
+--     require("nvim-treesitter.highlight").attach(0, "bash")
+--   end,
+-- })
+--
+--
+--
+-- #################################################################
+-- ########################## Go Setup #############################
+-- #################################################################
+
+------------------------
+-- Formatting
+------------------------
+-- formatters.setup {
+--   { command = "goimports", filetypes = { "go" } },
+--   { command = "gofumpt",   filetypes = { "go" } },
+-- }
+
+lvim.format_on_save = {
+  enabled = true,
+  pattern = { "*.go", "*.js", "*.rs", "*.py", "*.lua", ".yaml", "*.ts", "*.tsx", "*.jsx" },
+}
+
+------------------------
+-- Dap
+------------------------
+local dap_ok, dapgo = pcall(require, "dap-go")
+if not dap_ok then
+  return
+end
+
+dapgo.setup()
+
+------------------------
+-- LSP
+------------------------
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "gopls" })
+
+local lsp_manager = require "lvim.lsp.manager"
+lsp_manager.setup("golangci_lint_ls", {
+  on_init = require("lvim.lsp").common_on_init,
+  capabilities = require("lvim.lsp").common_capabilities(),
+})
+
+lsp_manager.setup("gopls", {
+  on_attach = function(client, bufnr)
+    require("lvim.lsp").common_on_attach(client, bufnr)
+    local _, _ = pcall(vim.lsp.codelens.refresh)
+    local map = function(mode, lhs, rhs, desc)
+      if desc then
+        desc = desc
+      end
+
+      vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc, buffer = bufnr, noremap = true })
+    end
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+    map("n", "<leader>zi", "<cmd>GoInstallDeps<Cr>", "Install Go Dependencies")
+    map("n", "<leader>zt", "<cmd>GoMod tidy<cr>", "Tidy")
+    map("n", "<leader>za", "<cmd>GoTestAdd<Cr>", "Add Test")
+    map("n", "<leader>zA", "<cmd>GoTestsAll<Cr>", "Add All Tests")
+    map("n", "<leader>ze", "<cmd>GoTestsExp<Cr>", "Add Exported Tests")
+    map("n", "<leader>zg", "<cmd>GoGenerate<Cr>", "Go Generate")
+    map("n", "<leader>zf", "<cmd>GoGenerate %<Cr>", "Go Generate File")
+    map("n", "<leader>zc", "<cmd>GoCmt<Cr>", "Generate Comment")
+    map("n", "<leader>zT", "<cmd>lua require('dap-go').debug_test()<cr>", "Debug Test")
+    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  end,
+  on_init = require("lvim.lsp").common_on_init,
+  capabilities = require("lvim.lsp").common_capabilities(),
+  settings = {
+    gopls = {
+      usePlaceholders = true,
+      gofumpt = true,
+      codelenses = {
+        generate = false,
+        gc_details = true,
+        test = true,
+        tidy = true,
+      },
+    },
+  },
+})
+
+-- lvim.keys.normal_mode["<leader>Bi"] = "<cmd>GoInstallDeps<Cr>"
+
+require("gopher").setup {
+  commands = {
+    go = "go",
+    gomodifytags = "gomodifytags",
+    gotests = "gotests",
+    impl = "impl",
+    iferr = "iferr",
+  },
+}
+
+local cmp_nvim_lsp = require "cmp_nvim_lsp"
+
+-- fixes: warning multiple different client offset encodings detected for buffer this is not supported yet
+-- https://www.reddit.com/r/neovim/comments/12qbcua/multiple_different_client_offset_encodings/
+
+local function clang_on_attach(client, bufnr)
+  -- Set up buffer-local keymaps for C/C++ development
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+
+  -- Go to definition
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+
+  -- Show hover information
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+
+  -- List references
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+
+  -- Rename symbol
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+
+  -- Show code actions
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+
+  -- Format buffer
+  vim.keymap.set('n', '<space>lf', function() vim.lsp.buf.format { async = true } end, bufopts)
+
+  -- Enable formatting on save
+  vim.api.nvim_create_autocmd('BufWritePre', {
+    buffer = bufnr,
+    callback = function()
+      vim.lsp.buf.format()
+    end,
+  })
+
+  -- Highlight symbol under cursor
+  if client.server_capabilities.documentHighlightProvider then
+    vim.api.nvim_create_augroup('lsp_document_highlight', { clear = true })
+    vim.api.nvim_clear_autocmds { group = 'lsp_document_highlight', buffer = bufnr }
+    vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+      group = 'lsp_document_highlight',
+      buffer = bufnr,
+      callback = vim.lsp.buf.document_highlight,
+    })
+    vim.api.nvim_create_autocmd('CursorMoved', {
+      group = 'lsp_document_highlight',
+      buffer = bufnr,
+      callback = vim.lsp.buf.clear_references,
+    })
+  end
+end
+
+-- Configure clangd
+require("lspconfig").clangd.setup {
+  on_attach = clang_on_attach,
+  capabilities = cmp_nvim_lsp.default_capabilities(),
+  cmd = {
+    "clangd",
+    "--offset-encoding=utf-16",
+  },
+}
+
+-- Pyright configs
+
+local pyright_opts = {
+  single_file_support = true,
+  settings = {
+    pyright = {
+      disableLanguageServices = false,
+      disableOrganizeImports = false
+    },
+    python = {
+      analysis = {
+        autoImportCompletions = true,
+        autoSearchPaths = true,
+        diagnosticMode = "workspace", -- openFilesOnly, workspace
+        typeCheckingMode = "off",     -- off, basic, strict
+        useLibraryCodeForTypes = true
+      }
+    }
+  },
+}
+
+require("lvim.lsp.manager").setup("pyright", pyright_opts)
---
+++ README.md
@@ -0,0 +1,13 @@
+## Apply dotfiles
+```
+cd ~/dotfiles
+stow .
+```
+## TMUX
+Start tmux using `-u` to enable colors and icons to render properly
+```
+tmux -u
+```
+
+`Ctrl + Space, I` to install plugins
+`tmux source ~/.config/tmux/tmux.conf` to source config changes
