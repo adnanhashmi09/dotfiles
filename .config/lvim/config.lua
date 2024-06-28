0c7b56b555e355281b1e83fd4dd3a03f4106bf83
Adnan Hashmi <hashmi.adnan791@gmail.com>
2024-06-28 16:43:47 +0530

    fix clipboard issue and implement code folding

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -18,6 +18,15 @@ lvim.format_on_save.enabled = true
 lvim.colorscheme = "lunar"
 lvim.transparent_window = true
 vim.diagnostic.config({ virtual_text = false })
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldcolumn = "0"
+vim.opt.foldtext = ""
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 1
+vim.opt.foldnestmax = 2
+
+
 
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
