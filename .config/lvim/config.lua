fd5b9865e83f295593dea234c486b9db497e6a28
adnanhashmi09 <hashmi.adnan791@gmail.com>
2024-10-08 14:12:51 +0530

    configs of lvim, skhd and tmux plugins

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -23,7 +23,7 @@ vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldcolumn = "0"
 vim.opt.foldtext = ""
 vim.opt.foldlevel = 99
-vim.opt.foldlevelstart = 1
+vim.opt.foldlevelstart = 99
 vim.opt.foldnestmax = 2
 
 
@@ -33,8 +33,8 @@ vim.opt.foldnestmax = 2
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
--- add your own keymapping
 lvim.number = false
+-- add your own keymapping
 
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["<S-l>"] = ":bnext<CR>"
@@ -236,22 +236,22 @@ formatters.setup {
 -- }
 
 -- -- set additional linters
--- local linters = require "lvim.lsp.null-ls.linters"
--- linters.setup {
--- { command = "flake8", filetypes = { "python" } },
--- {
---   -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
---   command = "shellcheck",
---   ---@usage arguments to pass to the formatter
---   -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
---   extra_args = { "--severity", "warning" },
--- },
--- {
---   command = "codespell",
---   ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
---   filetypes = { "javascript", "python" },
--- },
--- }
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+  { command = "flake8", filetypes = { "python" } },
+  -- {
+  --   -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+  --   command = "shellcheck",
+  --   ---@usage arguments to pass to the formatter
+  --   -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+  --   extra_args = { "--severity", "warning" },
+  -- },
+  -- {
+  --   command = "codespell",
+  --   ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+  --   filetypes = { "javascript", "python" },
+  -- },
+}
 
 -- Additional Plugins
 -- python environment switcher
